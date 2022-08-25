f0af6f2269107e2c15d215b441d00813984e115f
Jessie Hsieh <jessiehsieh86@gmail.com>
2022-08-25 14:41:23 +0800

    Add avg length

--- read.py
+++ read.py
@@ -8,7 +8,12 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0: # if count/1000 remainder is 0
 			print(len(data)) #print when read every 1000 data
 
-print(len(data)) #print how many reviews
-print(data[0]) #print first review
-print('---------------------------------')
-print(data[1])
+print('Finish reading', len(data), 'data') #print how many reviews
+
+sum_len = 0 #目前的留言總長度
+for d in data:
+	sum_len = sum_len + len(d) #目前已累積的留言總長度加上下一筆的留言長度
+print('The average length of review is', sum_len/len(data)) #需離開for loop才不會每一次加總的平均都算出來
+
+
+		
